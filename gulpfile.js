89e6f8f82dc2c6301272b56ef63b558ff4293453
Siarhei Davidouski <sergeyyellow@bk.ru>
2021-07-02 15:56:31 +0300

    Landing page 02.07.2021 15:56

---
+++ gulpfile.js
@@ -0,0 +1,109 @@
+'use strict';
+
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    prefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass')(require('sass')),
+    sourcemaps = require('gulp-sourcemap'),
+    rigger = require('gulp-rigger'),
+    cssmin = require('gulp-minify-css'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    rimraf = require('rimraf'),
+    browserSync = require("browser-sync"),
+    reload = browserSync.reload;
+
+var path = {
+    build: {
+        html: 'build/',
+        js: 'build/js/',
+        css: 'build/css/',
+        img: 'build/img/',
+        fonts: 'build/fonts/'
+    },
+    src: {
+        html: 'src/*.html',
+        js: 'src/js/script.js',
+        style: 'src/styles/style.scss',
+        img: 'src/img/**/*.*',
+        fonts: 'src/fonts/**/*.*'
+    },
+    watch: {
+        html: 'src/**/*.html',
+        js: 'src/js/**/*.js',
+        style: 'src/styles/**/*.scss',
+        img: 'src/img/**/*.*',
+        fonts: 'src/fonts/**/*.*'
+    },
+    clean: './build'
+};
+
+var config = {
+    server: {
+        baseDir: "./build"
+    },
+    tunnel: true,
+    host: 'localhost',
+    port: 3000,
+    logPrefix: "Marvel_Power_Group"
+};
+
+gulp.task('html:build', function () {
+    return gulp.src(path.src.html)
+        .pipe(rigger())
+        .pipe(gulp.dest(path.build.html))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('js:build', function () {
+    return gulp.src(path.src.js)
+        .pipe(rigger())
+        .pipe(uglify())
+        .pipe(gulp.dest(path.build.js))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('style:build', function () {
+    return gulp.src(path.src.style)
+        .pipe(sass())
+        .pipe(prefixer())
+        .pipe(cssmin())
+        .pipe(gulp.dest(path.build.css))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('image:build', function () {
+    return gulp.src(path.src.img)
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()],
+            interlaced: true
+        }))
+        .pipe(gulp.dest(path.build.img))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('fonts:build', function() {
+    return gulp.src(path.src.fonts)
+        .pipe(gulp.dest(path.build.fonts));
+});
+
+gulp.task('build', gulp.series('html:build', 'js:build', 'style:build', 'fonts:build', 'image:build'));
+
+gulp.task('webserver', function () {
+    browserSync(config);
+
+    watch([path.watch.html], gulp.series('html:build'));
+    watch([path.watch.style], gulp.series('style:build'));
+    watch([path.watch.js],gulp.series('js:build'));
+    watch([path.watch.img], gulp.series('image:build'));
+    watch([path.watch.fonts], gulp.series('fonts:build'));
+});
+
+gulp.task('clean', function (cb) {
+    rimraf(path.clean, cb);
+});
+
+gulp.task('default', gulp.series('clean', 'build', 'webserver'));
---
+++ readme.txt
@@ -0,0 +1,19 @@
+Sofia Pro:
+300 Book
+400 Regular
+600 Semi-bold, Demi-bold
+700 Bold
+
+Alata:
+400 Regular
+
+Types of fonts 📔:
+100: Thin, Hairline, Ultra-light;
+200: Extra-light;
+300: Book, Light;
+400: Regular, Normal, Plain, Roman, Standard;
+500: Medium;
+600: Semi-bold, Demi-bold;
+700: Bold;
+800: Heavy, Black, Extra-bold;
+900: Ultra-black, Extra-black, Ultra-bold, Heavy-black, Fat, Poster.
